8e17df006a557a1acad272033e3bada86d8f0688
Leozin777 <leo.cardoso@rede.ulbra.br>
2022-04-22 21:53:15 -0300

    aula8

---
+++ ConsoleAspNet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleAspNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+            .UseKestrel() 
+            .Configure
+            (
+                app => 
+                {
+                    app.Run(context => context.Response.WriteAsync("<h1>hello world<h1>"));
+                }
+            )
+            .Build();
+            host.Run();
+        }
+    }
+}
