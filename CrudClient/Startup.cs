823abe357430866596ca34b16705de1a19709313
Leozin777 <leo.cardoso@rede.ulbra.br>
2022-05-08 01:26:29 -0300

    crudclient

--- CrudClient/Startup.cs
+++ CrudClient/Startup.cs
@@ -2,19 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Aula11CrudPeople.Models.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace Aula11CrudPeople
+namespace CrudClient
 {
     public class Startup
     {
@@ -32,12 +30,8 @@ namespace Aula11CrudPeople
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Aula11CrudPeople", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CrudClient", Version = "v1" });
             });
-
-            services.AddDbContext<DataContext>(x=>x.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddScoped<IPersonRepository,PersonRepository>();
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,10 +41,10 @@ namespace Aula11CrudPeople
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Aula11CrudPeople v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CrudClient v1"));
             }
 
-           //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
